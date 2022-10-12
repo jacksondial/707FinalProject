a1021a37dfb5b59c6020a4a0b6e85ceacd152623
jacksondial <61251187+jacksondial@users.noreply.github.com>
2022-10-12 14:55:13 -0400

    Added read_data

---
+++ read_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+
+
+def read_data(filename=str):
+    pat_id = filename.rsplit("/", 1)[1].split(".")[0]
+    data = pd.read_table(filename, delimiter=",")
+    data = data.iloc[1:, :]
+    data["subjid"] = pd.Series([pat_id for x in range(len(data.index) + 1)])
+    return data
+
+
+appended_data = []
+with os.scandir("Data/mortality_prediction/mortality_prediction2/set-a/") as entries:
+    for entry in entries:
+        single_data = read_data(
+            f"Data/mortality_prediction/mortality_prediction2/set-a/{entry.name}"
+        )
+
+        appended_data.append(single_data)
+    appended_data_concat = pd.concat(appended_data)
+
+    new_df = appended_data_concat.pivot_table(
+        index=["subjid", "Time"], columns="Parameter", values="Value"
+    )
+    new_df.to_csv("Data/ADS.xlsx")
+    # print(new_df.head(10))
+
+# print(read_data("Data/mortality_prediction/mortality_prediction2/set-a/132539.txt"))
